81c69dff9f30d3d509970be659db3959d74f945f
silentmood <silentmood198909@gmail.com>
2016-04-18 13:40:50 +0800

    add the arguments type detect

--- index.js
+++ index.js
@@ -4,7 +4,24 @@
 let mqttConnection = require('mqtt-connection')
 let co = require('co')
 
+function isGenerator(obj) {
+  return 'function' == typeof obj.next && 'function' == typeof obj.throw;
+}
+
+function isGeneratorFunction(obj) {
+  var constructor = obj.constructor;
+  if (!constructor) return false;
+  if ('GeneratorFunction' === constructor.name || 'GeneratorFunction' === constructor.displayName) return true;
+  return isGenerator(constructor.prototype);
+}
+
 mqttConnection.prototype.onx = function (ev, gen, onDone, onError) {
+  if(!gen || !(isGeneratorFunction(gen) || isGenerator(gen))) {
+    if (!!onError && typeof onError === 'function') {
+      onError(new Error('onx function should accept generator function'))
+    }
+    return
+  }
   this.on(ev, () => {
     co(gen).then((data) => {
       if (!!onDone && typeof onDone === 'function')
